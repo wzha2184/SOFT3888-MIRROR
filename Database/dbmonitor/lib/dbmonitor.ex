2cbb6d110c5a59a9067550e433d620082b37b194
Ehab_El_Cheikh <ehel9045@uni.sydney.edu.au>
2022-08-30 12:36:59 +1000

    Testing pull request

--- Database/dbmonitor/lib/dbmonitor.ex
+++ Database/dbmonitor/lib/dbmonitor.ex
@@ -3,15 +3,6 @@ defmodule Dbmonitor do
   Documentation for `Dbmonitor`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dbmonitor.hello()
-      :world
-
-  """
   def func do
     Postgrex.start_link(hostname: "localhost", username: "postgres", password: "postgres", database: "postgres")
   end
