d6fbb5d9e49bad9852a38db186b26d4f6dec4c33
Ehab_El_Cheikh <ehel9045@uni.sydney.edu.au>
2022-09-02 19:38:40 +1000

    Added function to insert Data

--- Database/dbmonitor/lib/dbmonitor.ex
+++ Database/dbmonitor/lib/dbmonitor.ex
@@ -2,11 +2,20 @@ defmodule Dbmonitor do
   @moduledoc """
   Documentation for `Dbmonitor`.
   """
-  defmodule Dbmonitor.Criticalsens do
     use Ecto.Schema
+    alias Dbmonitor.Repo
     schema "criticalsens" do
       field :cpufan, :string
       field :cpuopt, :string, default: "0"
     end
+
+    def insertS do
+      a = %Dbmonitor{cpufan: "1000", cpuopt: "0"}
+      Repo.insert(a)
+    end
   end
-end
+  # Example of insertion:
+  # Create data and store as a map:
+  #   a = %Dbmonitor.Dbmonitor.Criticalsens{cpufan: "1000", cpuopt: "0"}
+  # Call insertion command:
+  #   Dbmonitor.Repo.insert(a)
