a28af737b56e11fa43b3e58782e4c2e90204c2c6
Ehab_El_Cheikh <ehel9045@uni.sydney.edu.au>
2022-09-04 12:22:19 +1000

    Added basic select query

--- Database/dbmonitor/README.md
+++ Database/dbmonitor/README.md
@@ -19,3 +19,6 @@ Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_do
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/dbmonitor>.
 
+To run the program run:
+  1. mix deps.get
+  2. mix ecto.migrate
--- Database/dbmonitor/lib/dbmonitor.ex
+++ Database/dbmonitor/lib/dbmonitor.ex
@@ -4,20 +4,27 @@ defmodule Dbmonitor do
   """
     # Example of insertion from terminal:
     # Create data and store as a map:
-    #   a = %Dbmonitor.Dbmonitor.Criticalsens{cpufan: "1000", cpuopt: "0"}
+    #   a = %Dbmonitor.Criticalsens{cpufan: "1000", cpuopt: "0"}
     # Call insertion command:
     #   Dbmonitor.Repo.insert(a)
   defmodule Criticalsens do
     use Ecto.Schema
     alias Dbmonitor.Repo
+    import Ecto.Query
     schema "criticalsens" do
       field :cpufan, :string
       field :cpuopt, :string, default: "0"
     end
 
-    def insertS do
+    def insertCriticalSens do
       a = %Criticalsens{cpufan: "2000", cpuopt: "2300"}
-      Repo.insert(a)
+      Repo.insert!(a)
+    end
+
+    def getAllData do
+      query = from u in "criticalsens",
+      select: {u.cpufan, u.cpuopt}
+      Repo.all(query)
     end
   end
 end
