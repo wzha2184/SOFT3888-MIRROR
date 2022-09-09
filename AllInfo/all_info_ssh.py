997a492080f9b06445dccc3b4f1f4005a76abd09
Alan Chen <yche2656@uni.sydney.edu.au>
2022-09-09 08:31:32 +0000

    establish an ssh connection with supercluster 9 and get all info from it

---
+++ AllInfo/all_info_ssh.py
@@ -0,0 +1,7 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect("192.168.10.9", username="usyd-10a", password="6r7mYcxLHXLq8Rgu")
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('cd AllInfo; echo 6r7mYcxLHXLq8Rgu | sudo -S -k python3 all_info.py')
+print(ssh_stdout.read().decode('utf-8'))
