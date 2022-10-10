ab3c7eb6edd9dc2101c789ea1f720865204e65dc
Black Hole <rwan4990@uni.sydney.edu.au>
2022-10-10 15:18:07 +1100

    new bot update

--- Discord_BOT/AlertBOT.py
+++ Discord_BOT/AlertBOT.py
@@ -1,6 +1,10 @@
 import discord
 import random
 from discord.ext import tasks
+#change flowing two attributes
+channel_ID = 1028168592124026882
+token = 'MTAyODE2NzY5MDA3MTgzODcyMA.G2_bMN.T9H_gZt3oUwfGKuCtIedDudXUPd0rfmNHrS5MM'
+
 text = ""
 def run(report):
   client = discord.Client(intents=discord.Intents.default())
@@ -10,10 +14,12 @@ def run(report):
   @client.event
   async def on_ready():
       global text
-      channel = client.get_channel(1028168592124026882)
+      global channel_ID
+      channel = client.get_channel(channel_ID)
       await channel.send(text)
       await client.close()
-    
-  client.run('MTAyODE2NzY5MDA3MTgzODcyMA.G2_bMN.T9H_gZt3oUwfGKuCtIedDudXUPd0rfmNHrS5MM')
+  
+  global token
+  client.run(token)
 
-run("Report")
+#run("Report") will send "Report"
