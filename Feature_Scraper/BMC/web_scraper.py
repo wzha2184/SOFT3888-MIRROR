8143fdaf50f4824c7f6dbec67083e73bb3fdf3a2
Astral_Jason <jliu9493@uni.sydney.edu.au>
2022-10-11 20:35:55 +1100

    update shell scraper

--- Feature_Scraper/BMC/web_scraper.py
+++ Feature_Scraper/BMC/web_scraper.py
@@ -6,10 +6,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from bs4 import BeautifulSoup
 import json
+import os
 
 class WebAccesser:
-    def __init__(self, BMC_url: str) -> None:
-        self.url = BMC_url
+    def __init__(self) -> None:
+        self.url = ""
 
         self._options = webdriver.ChromeOptions()
         self._options.add_argument('--no-sandbox')
@@ -19,6 +20,9 @@ class WebAccesser:
 
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self._options)
         
+    def set_url(self, url: str) -> None:
+        self.url = url
+    
     def login(self) -> None:
         self.driver.get(self.url + '/#login')
         
@@ -33,81 +37,107 @@ class WebAccesser:
         
         time.sleep(15)
 
-    def get_page(self) -> str:
-        self.driver.get(self.url + '/#sensors')
+    def get_page(self, page_name: str) -> str:
+        self.driver.get(self.url + '/#{}'.format(page_name))
         time.sleep(10)
         return str(self.driver.page_source)
 
 
 class WebScraper:
-    def __init__(self, machine_name: str, BMC_url: str) -> None:
-        self.web_acceccer = WebAccesser(BMC_url)
+    def __init__(self, machine_name: str, web_accesser: WebAccesser) -> None:
+        self.web_accesser = web_accesser
         self.machin_name = machine_name
 
         self.result = {}
-        self.result["BMC"] = {}
-        self.result["BMC"][machine_name] = {}
 
-        self.get_cpu_info()
+        self.get_info()
 
     def get_bmc_result(self) -> dict:
         return self.result 
 
-    def get_cpu_info(self) -> None:
-        self.web_acceccer.login()
-        html_doc = self.web_acceccer.get_page()
+    def get_info(self) -> None:
+        try:
+            self.web_accesser.login()
+            html_doc = self.web_accesser.get_page()
 
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        critical_sensors = None
-        descrete_sensors = None
-        normal_sensors = None
+            self.result["status"] = "OK"
 
-        info = soup.find_all("div")
-        for div in info:
-            if div.get("class") is not None and " ".join(div.get("class")) == "row animated fadeInUp":
-                critical_sensors = div
-            elif div.get("class") is not None and " ".join(div.get("class")) == "row animated fadeInUp delay-0":
-                descrete_sensors = div
-            elif div.get("class") is not None and " ".join(div.get("class")) == "row animated fadeInUp delay-1":
-                normal_sensors = div
+            soup = BeautifulSoup(html_doc, 'html.parser')
+            critical_sensors = None
+            descrete_sensors = None
+            normal_sensors = None
 
-        # Critical sensors(CPU)
-        CPU_info = critical_sensors.find_all("div")
-        name = None
-        value = None
-        for div in CPU_info:
-            if " ".join(div.get("class")) == "percentage":
-                value = div.text
-            if " ".join(div.get("class")) == "sensor-title":
-                name = div.text
+            info = soup.find_all("div")
+            for div in info:
+                if div.get("class") is not None and " ".join(div.get("class")) == "row animated fadeInUp":
+                    critical_sensors = div
+                elif div.get("class") is not None and " ".join(div.get("class")) == "row animated fadeInUp delay-0":
+                    descrete_sensors = div
+                elif div.get("class") is not None and " ".join(div.get("class")) == "row animated fadeInUp delay-1":
+                    normal_sensors = div
 
-            if name is not None and value is not None:
-                self.result["BMC"][self.machin_name][name] = value.split(" ")[0].replace("RPM", "")
-                name = None
-                value = None
+            # Critical sensors(CPU)
+            CPU_info = critical_sensors.find_all("div")
+            name = None
+            value = None
+            for div in CPU_info:
+                if " ".join(div.get("class")) == "percentage":
+                    value = div.text
+                if " ".join(div.get("class")) == "sensor-title":
+                    name = div.text
 
-        # Descrete sensors
-        descrete_info = descrete_sensors.find_all("tr")
-        for row in descrete_info:
-            state = []
-            for col in row.find_all("td"):
-                if col.get("class") is None or "".join(col.get("class")) != "hide":
+                if name is not None and value is not None:
+                    self.result[name] = value.split(" ")[0].replace("RPM", "")
+                    name = None
+                    value = None
+
+            # Descrete sensors
+            descrete_info = descrete_sensors.find_all("tr")
+            for row in descrete_info:
+                state = []
+                for col in row.find_all("td"):
+                    if col.get("class") is None or "".join(col.get("class")) != "hide":
+                        state.append(col.text.strip())
+                    if len(state) == 2:
+                        self.result[state[0]] = state[1].split(" ")[0].replace("RPM", "")
+                        state = []
+
+            normal_info = normal_sensors.find_all("tr")
+            for row in normal_info:
+                state = []
+                for col in row.find_all("td"):
                     state.append(col.text.strip())
-                if len(state) == 2:
-                    self.result["BMC"][self.machin_name][state[0]] = state[1].split(" ")[0].replace("RPM", "")
-                    state = []
+                    if len(state) == 2:
+                        self.result[state[0]] = state[1].split(" ")[0].replace("RPM", "")
+                        state = []
+        except:
+            self.result["status"] = "error"
 
-        normal_info = normal_sensors.find_all("tr")
-        for row in normal_info:
-            state = []
-            for col in row.find_all("td"):
-                state.append(col.text.strip())
-                if len(state) == 2:
-                    self.result["BMC"][self.machin_name][state[0]] = state[1].split(" ")[0].replace("RPM", "")
-                    state = []
+def run(url_config: str) -> dict:
+    with open(url_config, "r") as jc:
+        config = json.load(jc)
+        superclusters = config["superclusters"]
 
+        web_accesser = WebAccesser()
+        result = {}
+        result["BMC"] = {}
+        for sc in superclusters.keys():
+            bmc_url = superclusters[sc]["BMC"]
+            web_accesser.set_url(bmc_url)
+            web_scraper = WebScraper(sc, web_accesser)
+    
+            result["BMC"][sc] = web_scraper.get_bmc_result()
+
+        return result
 
 if __name__ == "__main__":
-    web_scraper = WebScraper("BMC1", "https://192.168.10.108")
-    print(json.dumps(web_scraper.get_bmc_result(), indent=2))
+    # config = os.path.join("..", "config.json")
+    # result = run(config)
+    # print(json.dumps(result, indent=2))
+    web_accesser = WebAccesser()
+    bmc_url = "https://192.168.10.102"
+    web_accesser.set_url(bmc_url)
+
+    web_accesser.login()
+    web_accesser.get_page()
 
--- Feature_Scraper/Supercluster/gpu_scraper.py
+++ Feature_Scraper/Supercluster/gpu_scraper.py
@@ -6,9 +6,7 @@ from pynvml import *
 class GPUScraper:
     def __init__(self) -> None:
         nvmlInit()
-
         self.result = {}
-        self.result["GPU"] = {}
 
     def get_count(self) -> int:
         return igpu.count_devices()
@@ -19,53 +17,56 @@ class GPUScraper:
 
     def get_gpu_info(self, num) -> None:
         gpu = igpu.get_device(num)
-        self.result["GPU"][gpu.uuid] = {}
-        self.result["GPU"][gpu.uuid]["UUID"] = str(gpu.uuid)
-        self.result["GPU"][gpu.uuid]["BIOS"] = str(gpu.bios)
-        self.result["GPU"][gpu.uuid]["Name"] = str(gpu.name)
-        self.result["GPU"][gpu.uuid]["Temperature"] = str(gpu.utilization.temperature)
+        self.result[gpu.uuid]["UUID"] = str(gpu.uuid)
+        self.result[gpu.uuid]["BIOS"] = str(gpu.bios)
+        self.result[gpu.uuid]["Name"] = str(gpu.name)
+        self.result[gpu.uuid]["Temperature"] = str(gpu.utilization.temperature)
     
     def get_gpu_memory(self, num) -> None:
         gpu_info = igpu.get_device(num)
-        self.result["GPU"][gpu_info.uuid]["totalMemory"] = str(gpu_info.memory.total)
-        self.result["GPU"][gpu_info.uuid]["usedMemory"] = str(gpu_info.memory.used)
-        self.result["GPU"][gpu_info.uuid]["freeMemory"] = str(gpu_info.memory.free)
+        self.result[gpu_info.uuid]["totalMemory"] = str(gpu_info.memory.total)
+        self.result[gpu_info.uuid]["usedMemory"] = str(gpu_info.memory.used)
+        self.result[gpu_info.uuid]["freeMemory"] = str(gpu_info.memory.free)
         
     def get_gpu_power(self, num) -> None:
         uuid = igpu.get_device(num).uuid
         gpu_info_power = igpu.get_device(num).power
-        self.result["GPU"][uuid]["power"] = str(gpu_info_power.draw)
-        self.result["GPU"][uuid]["limitPower"] = str(gpu_info_power.limit)
-        self.result["GPU"][uuid]["minLimitPower"] = str(gpu_info_power.min_limit)
-        self.result["GPU"][uuid]["maxLimitPower"] = str(gpu_info_power.max_limit)
+        self.result[uuid]["power"] = str(gpu_info_power.draw)
+        self.result[uuid]["limitPower"] = str(gpu_info_power.limit)
+        self.result[uuid]["minLimitPower"] = str(gpu_info_power.min_limit)
+        self.result[uuid]["maxLimitPower"] = str(gpu_info_power.max_limit)
 
     def get_gpu_frequency(self, num) -> None:
         gpu_info = igpu.get_device(num)
-        self.result["GPU"][gpu_info.uuid]["graphicsFrequency"] = str(gpu_info.clocks.graphics)
-        self.result["GPU"][gpu_info.uuid]["StreamingMultiprocessorFrequency"] = str(gpu_info.clocks.sm)
-        self.result["GPU"][gpu_info.uuid]["memoryFrequency"] = str(gpu_info.clocks.memory)
-        self.result["GPU"][gpu_info.uuid]["MaxgraphicsFrequency"] = str(gpu_info.clocks.max_graphics)
-        self.result["GPU"][gpu_info.uuid]["MaxStreamingMultiprocessorFrequency"] = str(gpu_info.clocks.max_sm)
-        self.result["GPU"][gpu_info.uuid]["MaxmemoryFrequency"] = str(gpu_info.clocks.max_memory)
+        self.result[gpu_info.uuid]["graphicsFrequency"] = str(gpu_info.clocks.graphics)
+        self.result[gpu_info.uuid]["StreamingMultiprocessorFrequency"] = str(gpu_info.clocks.sm)
+        self.result[gpu_info.uuid]["memoryFrequency"] = str(gpu_info.clocks.memory)
+        self.result[gpu_info.uuid]["MaxgraphicsFrequency"] = str(gpu_info.clocks.max_graphics)
+        self.result[gpu_info.uuid]["MaxStreamingMultiprocessorFrequency"] = str(gpu_info.clocks.max_sm)
+        self.result[gpu_info.uuid]["MaxmemoryFrequency"] = str(gpu_info.clocks.max_memory)
 
     def get_gpu_fan(self, num) -> None:
         gpu_info = igpu.get_device(num)
         try:
             handle = nvmlDeviceGetHandleByIndex(num)
             fan_speed = nvmlDeviceGetFanSpeed(handle)
-            self.result["GPU"][gpu_info.uuid]["fanSpeed"] = fan_speed
+            self.result[gpu_info.uuid]["fanSpeed"] = fan_speed
         except:
-            self.result["GPU"][gpu_info.uuid]["fanSpeed"] = "0"
+            self.result[gpu_info.uuid]["fanSpeed"] = "0"
 
     def get_all_gpu_result(self) -> None:
-        self.result["GPU"]["GPU_count"] = self.get_count()
+        self.result["GPU_count"] = self.get_count()
         for i in range(self.get_count()):
-            self.result["GPU"][igpu.get_device(i).uuid] = {}
-            self.get_gpu_info(i)
-            self.get_gpu_memory(i)
-            self.get_gpu_power(i)
-            self.get_gpu_frequency(i)
-            self.get_gpu_fan(i)
+            self.result[igpu.get_device(i).uuid] = {}
+            try:
+                self.get_gpu_info(i)
+                self.get_gpu_memory(i)
+                self.get_gpu_power(i)
+                self.get_gpu_frequency(i)
+                self.get_gpu_fan(i)
+                self.result[igpu.get_device(i).uuid]["status"] = "OK"
+            except:
+                self.result[igpu.get_device(i).uuid]["status"] = "error"
 
 
 if __name__ == "__main__":
