d6962f63fefc8dd80bbbf45113b52a67cf12a5c1
Alan Chen <yche2656@uni.sydney.edu.au>
2022-10-16 05:09:40 +0000

    add test_gpu_scraper.py to test gpu_scraper.py

---
+++ Feature_Scraper/Supercluster/test_gpu_scraper.py
@@ -0,0 +1,47 @@
+from gpu_scraper import GPUScraper
+
+gpu = GPUScraper()
+result = gpu.get_gpu_result()
+
+assert 'GPU_count' in result
+assert result['GPU_count'] == 2
+board1 = 'GPU-d1beb192-bcec-67d2-4a5f-51108e4f03d1'
+board2 = 'GPU-70987dcb-d543-54bc-8ac5-fc3cfc530043'
+assert board1 in result
+assert board2 in result
+
+target_attributes = ['UUID', "BIOS", "Name", "Temperature", 'totalMemory', 'usedMemory', 'freeMemory', 'power', 'limitPower', 'minLimitPower', 'maxLimitPower', 'graphicsFrequency', 'StreamingMultiprocessorFrequency', 'memoryFrequency', 'MaxgraphicsFrequency', 'MaxStreamingMultiprocessorFrequency', 'MaxmemoryFrequency', 'fanSpeed', 'status']
+for t in target_attributes:
+    assert t in result[board1]
+    assert t in result[board2]
+
+assert result[board1]['Name'] == 'NVIDIA GeForce RTX 3090'
+assert result[board2]['Name'] == 'NVIDIA GeForce RTX 3090'
+
+one_decimal_place_attributes = ['Temperature', 'totalMemory', 'limitPower', 'minLimitPower', 'maxLimitPower', 'graphicsFrequency', 'StreamingMultiprocessorFrequency', 'memoryFrequency', 'MaxgraphicsFrequency', 'MaxStreamingMultiprocessorFrequency', 'MaxmemoryFrequency']
+for o in one_decimal_place_attributes:
+    assert result[board1][o][-2] == '.'
+    assert result[board1][o][:-2].isdigit()
+    assert result[board1][o][-1].isdigit()
+    assert result[board2][o][-2] == '.'
+    assert result[board2][o][:-2].isdigit()
+    assert result[board2][o][-1].isdigit()
+
+four_decimal_places_attributes = ['usedMemory', 'freeMemory']
+for f in four_decimal_places_attributes:
+    assert result[board1][o][-5] == '.'
+    assert result[board1][o][:-5].isdigit()
+    assert result[board1][o][-4:].isdigit()
+    assert result[board2][o][-5] == '.'
+    assert result[board2][o][:-5].isdigit()
+    assert result[board2][o][-4:].isdigit()
+
+assert result[board1]['power'][-4] == '.'
+assert result[board1]['power'][:-4].isdigit()
+assert result[board1]['power'][-3:].isdigit()
+assert result[board2]['power'][-4] == '.'
+assert result[board2]['power'][:-4].isdigit()
+assert result[board2]['power'][-3:].isdigit()
+
+assert result[board1]['fanSpeed'].isdigit()
+assert result[board1]['fanSpeed'].isdigit()
