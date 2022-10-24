3f9c2f8a3fd9eaaf5a353472c280b203ec93b8d2
Alan Chen <yche2656@uni.sydney.edu.au>
2022-10-24 09:51:27 +0000

    test_gpu_scraper.py fix small bug

--- Feature_Scraper/Supercluster/test_gpu_scraper.py
+++ Feature_Scraper/Supercluster/test_gpu_scraper.py
@@ -5,11 +5,10 @@ result = gpu.get_gpu_result()
 
 assert 'GPU_count' in result
 assert result['GPU_count'] == 2
-board1 = 'GPU-d1beb192-bcec-67d2-4a5f-51108e4f03d1'
-board2 = 'GPU-70987dcb-d543-54bc-8ac5-fc3cfc530043'
-assert board1 in result
-assert board2 in result
-print("Check the output contains 2 correct boards -- OK")
+print("Check the output contains 2 boards -- OK")
+
+board1 = '1'
+board2 = '2'
 
 target_attributes = ['UUID', "BIOS", "Name", "Temperature", 'totalMemory', 'usedMemory', 'freeMemory', 'power', 'limitPower', 'minLimitPower', 'maxLimitPower', 'graphicsFrequency', 'StreamingMultiprocessorFrequency', 'memoryFrequency', 'MaxgraphicsFrequency', 'MaxStreamingMultiprocessorFrequency', 'MaxmemoryFrequency', 'fanSpeed', 'status']
 for t in target_attributes:
@@ -17,8 +16,8 @@ for t in target_attributes:
     assert t in result[board2]
 print("Check all attributes in 2 boards' outputs -- OK")
 
-assert result[board1]['Name'] == 'NVIDIA GeForce RTX 3090'
-assert result[board2]['Name'] == 'NVIDIA GeForce RTX 3090'
+assert len(result[board1]['Name']) > 0
+assert len(result[board2]['Name']) > 0
 print("Check Name of the 2 GPU boards -- OK")
 
 one_decimal_place_attributes = ['Temperature', 'totalMemory', 'limitPower', 'minLimitPower', 'maxLimitPower', 'graphicsFrequency', 'StreamingMultiprocessorFrequency', 'memoryFrequency', 'MaxgraphicsFrequency', 'MaxStreamingMultiprocessorFrequency', 'MaxmemoryFrequency']
