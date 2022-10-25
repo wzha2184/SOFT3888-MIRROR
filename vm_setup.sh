0b26735d61c433e212451c0332c7fbd37c75ab54
Astral_Jason <jliu9493@uni.sydney.edu.au>
2022-10-26 02:19:16 +1100

    add environment setup script

---
+++ vm_setup.sh
@@ -0,0 +1,43 @@
+# Updata Ubuntu
+sudo apt update
+
+# Git Installation
+sudo apt-get install git
+
+# Elixir Installation
+sudo apt-get install elixir
+
+# Python Installation
+sudo apt install python3-pip
+
+# Install Elixir Mix Package
+sudo mix dept.get
+
+# Install Python igpu package
+sudo pip install igpu
+
+# Install Chrome
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Install Python Selenium package
+sudo pip install selenium
+
+# Install Python BeautifulSoup package
+sudo pip install beautifulsoup4
+
+# Install Python pynvml package
+sudo pip install pynvml
+
+# Install Python psutil package
+sudo pip install psutil
+
+# Install Python subprocess package
+sudo pip install subprocess.run
+
+# Install Python paramiko package
+sudo pip install paramiko
+
+# Install Python discord package
+sudo pip install discord
+
